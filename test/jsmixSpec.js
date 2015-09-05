256f8a7dfcf08e58131ae352daf10d7f96a2d1ad
Balazs Korossy <khayll@Balazss-MacBook-Air.local>
2015-09-05 11:52:28 +0200

    added test case for JSON input

--- test/jsmixSpec.js
+++ test/jsmixSpec.js
@@ -26,4 +26,8 @@ describe("JSMix", function() {
 	  var result = JSMix(sampleData).withObject(Department.prototype, "department").build();
     expect(result.department.getTime()).toContain("Time at Center of the World is");
   });
+  it("can also handle a JSON string", function() {
+	  var result = JSMix('{"department":{"location":"here"}}').withObject(Department.prototype, "department").build();
+    expect(result.department.getTime()).toContain("Time at here is");
+  });
 });
