7f3103aa53ceb285c93d7a12eadd9cc8952a08b4
Balázs Korossy <khayll@Balazss-iMac.local>
2015-09-05 21:18:53 +0200

    nested objects support added

---
+++ test/helpers/Pet.js
@@ -0,0 +1,9 @@
+/**
+ * Pet class for the tests 
+ */
+var Pet = function() {
+}
+
+Pet.prototype.getLike = function() {
+	return this.name + ' likes ' + this.like;
+}
--- test/jsmixSpec.js
+++ test/jsmixSpec.js
@@ -5,13 +5,21 @@ var sampleData = {
 			"firstName": "John",
 			"lastName": "Doe",
 			"salary": 100000,
-			"age": 33	
+			"age": 33,
+			"pet": {
+				name: "Buggs Bunny",
+				like: "carrot"
+			}
 		},
 		{
 			"firstName": "Romeo",
 			"lastName": "Alfa",
 			"salary": 150000,
-			"age": 34
+			"age": 34,
+			"pet": {
+				name: "Jerry",
+				like: "cheese"
+			}
 		}
 	],
 	"department": {
@@ -26,8 +34,25 @@ describe("JSMix", function() {
 	  var result = JSMix(sampleData).withObject(Department.prototype, "department").build();
     expect(result.department.getTime()).toContain("Time at Center of the World is");
   });
+
   it("can also handle a JSON string", function() {
 	  var result = JSMix('{"department":{"location":"here"}}').withObject(Department.prototype, "department").build();
     expect(result.department.getTime()).toContain("Time at here is");
   });
+
+  it("Mix array objects", function() {
+	  var result = JSMix(sampleData).withObject(Employee.prototype, "employees").build();
+    expect(result.employees[0].getName()).toContain("John Doe");
+  });
+
+  it("Mix every object in root object ", function() {
+	  var result = JSMix(sampleData).withObject(Department.prototype, "*").build();
+    expect(result.employees.getTime()).toContain("Time at undefined is");
+  });
+
+  it("Mix non toplevel data", function() {
+	  var result = JSMix(sampleData).withObject(Pet.prototype, "employees.*.pet").build();
+    expect(result.employees[1].pet.getLike()).toContain("Jerry likes cheese");
+  });
+
 });
